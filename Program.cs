af17c31506a6c6bc799b17d0e952c6af680eeb3e
FatmaGaberTolba <46868063+FatmaGaberTolba@users.noreply.github.com>
2020-01-15 13:23:38 +0300

    Add files via upload
    HackerRank Append and Delete Solution c# solution

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoImproveYouSelf
+{
+    class Program
+    {
+
+
+        // Complete the appendAndDelete function below.
+        static string appendAndDelete(string s, string t, int k)
+        {
+            var end = 0;
+
+            for (var i = 0; i < Math.Min(t.Length, s.Length); i++)
+            {
+                if (s[i] == t[i])
+                {
+                    end++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            var maxIter = s.Length + t.Length;
+            var minIter = maxIter - (2 * end);
+
+
+            if ((k >= minIter) & ((k - minIter) % 2 == 0))
+            {
+                return "Yes";
+            }
+            else if (k >= maxIter)
+            {
+                return "Yes";
+            }
+            else
+            {
+                return "No";
+            }
+    
+        }
+
+        static void Main(string[] args)
+        {
+
+            string s = "hackerhappy";
+
+            string t = "hackerrank";
+
+            int k = 7;
+
+            string result = appendAndDelete(s, t, k);
+
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
